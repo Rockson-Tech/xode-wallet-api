9fc1671e5f3c72a89054596fe8b99018b3971846
Joel Villena <villenajv43@gmail.com>
2024-03-10 16:05:30 +0800

    change var name

--- src/app.ts
+++ src/app.ts
@@ -75,7 +75,7 @@ const app: FastifyPluginAsync<AppOptions> = async (
       info: {
         title: 'XODE Wallet',
         description: 'Fastify swagger of XODE to smart contract.\n\n' +
-        '\n' + 'AstroChibbi: ' + process.env.ASTROCHIBBI_ADDRESS as string +
+        '\n' + 'AstroChibbi: ' + process.env.CONTRACT_ADDRESS as string +
         '\n' + 'Energy Capsule: ' + process.env.ENERGY_ADDRESS as string +
         '\n' + 'Astro Economy: ' + process.env.ECONOMY_ADDRESS as string,
         version: '0.0.2'
