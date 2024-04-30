f92c7eb08ca250e5aa95e1febb83d6f035d51e85
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2024-04-30 15:23:14 +0800

    get AZK, ASTRO,XGM and XAV balances

--- src/controllers/ChainController.ts
+++ src/controllers/ChainController.ts
@@ -3,6 +3,9 @@ import ChainRepository from '../repositories/ChainRepository';
 import AstroRepository from '../repositories/AstroRepository';
 import { ITokensRequestParams } from '../schemas/ChainSchemas';
 import WebsocketHeader from '../modules/WebsocketHeader';
+import AzkalRepository from '../repositories/AzkalRepository';
+import XGameRepository from '../repositories/XGameRepository';
+import XaverRepository from '../repositories/XaverRepository';
 
 // Get smart contract
 export const getSmartContractController = async (
@@ -47,15 +50,21 @@ export const getTokensController = async (
             return reply.badRequest("Invalid request parameter. Required fields: 'wallet_address'");
         }
         
-        let tokens = [];
-        const native = await ChainRepository.getTokensRepo(requestParams.wallet_address);
-        const astro = await AstroRepository.balanceOfRepo(requestParams.wallet_address);
-        if (native instanceof Error || astro instanceof Error) {
-            throw native || astro;
-        }
-        tokens.push(native);
-        tokens.push(astro);
-        return await reply.send(tokens);
+        // let tokens = [];
+        // const native = await ChainRepository.getTokensRepo(requestParams.wallet_address);
+        // const astro = await AstroRepository.balanceOfRepo(requestParams.wallet_address);
+        // if (native instanceof Error || astro instanceof Error) {
+        //     throw native || astro;
+        // }
+        const native = await Promise.all([
+            ChainRepository.getTokensRepo(requestParams.wallet_address),
+            AstroRepository.balanceOfRepo(requestParams.wallet_address),
+            AzkalRepository.balanceOfRepo(requestParams.wallet_address),
+            XGameRepository.balanceOfRepo(requestParams.wallet_address),
+            XaverRepository.balanceOfRepo(requestParams.wallet_address)
+        ])
+        // return await reply.send(tokens);
+        return await reply.send(native);
     } catch (error: any) {
         reply.status(500).send('Internal Server Error: ' + error);
     }
