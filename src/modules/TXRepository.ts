f837693751b7c23cfef25d91d95c790867a3f98c
Joel Villena <villenajv43@gmail.com>
2024-04-30 11:20:06 +0800

    remove finalize

--- src/modules/TXRepository.ts
+++ src/modules/TXRepository.ts
@@ -12,16 +12,6 @@ export default class TXRepository {
         }
     }
     
-    static isBroadcast = async (param: boolean) => {
-        let result: boolean = true;
-        if (param) {
-            result = param;
-        } else {
-            result = false;
-        }
-        return result;
-    }
-    
     static sendApiTransaction = async (
         api: any,
         pallet: any,
@@ -31,41 +21,30 @@ export default class TXRepository {
     ) => {
         return new Promise(async (resolve, reject) => {
             try {
-                // const nonce = await api.rpc.system.accountNextIndex(owner.address);
-                await api.tx[pallet][method](
+                const dryRunResult = await this.dryRunTransaction(api, pallet, method, owner, params);
+                if (!dryRunResult) {
+                    reject(dryRunResult);
+                }
+                const tx =  api.tx[pallet][method](
                 ...params
-                ).signAndSend(owner, { nonce: -1 }, async (result: any) => {
-                    console.log(result.status.toHuman());
-                    const isBroadcast = await this.isBroadcast(result.status.isBroadcast);
+                )
+                await await tx.signAndSend(owner, { nonce: -1 }, async (result: any) => {
                     if (result.dispatchError) {
                         if (result.dispatchError.isModule) {
                             const decoded = api.registry.findMetaError(result.dispatchError.asModule);
                             const { docs, name, section } = decoded;
-                            console.log(`${section}.${name}: ${docs.join(' ')}`);
                             reject(`${section}.${name}: ${docs.join(' ')}`);
                         } else {
                             console.log(result.dispatchError.toString());
                             reject(result.dispatchError.toString());
                         }
                     } else if (result.status.isInBlock) {
-                        if (!isBroadcast) {
-                            resolve({
-                                status: 200,
-                                message: `${method} in block`,
-                                data: {
-                                    isFinalized: result.status.isFinalized,
-                                    blockHash: result.status.asInBlock.toHex(),
-                                },
-                            });
-                        }
-                    } else if (result.status.isFinalized) {
-                        console.log('finalized');
                         resolve({
                             status: 200,
-                            message: `${method} finalized`,
+                            message: `${method} in block`,
                             data: {
                                 isFinalized: result.status.isFinalized,
-                                blockHash: result.status.asFinalized.toHex(),
+                                blockHash: result.status.asInBlock.toHex(),
                             },
                         });
                     }
@@ -87,19 +66,21 @@ export default class TXRepository {
     ) => {
         return new Promise(async (resolve, reject) => {
             try {
-                // const nonce = await api.rpc.system.accountNextIndex(owner.address);
-                await contract.tx[method](
+                const dryRunResult = await this.dryRunContract(api, contract, method, owner, params, instance, storageDepositLimit);
+                if (!dryRunResult) {
+                    reject(dryRunResult);
+                }
+                const tx =  contract.tx[method](
                 {
                     storageDepositLimit,
                     gasLimit: api?.registry.createType('WeightV2', {
-                        refTime: instance.REFTIME,
-                        proofSize: instance.PROOFSIZE,
+                        refTime: parseInt(dryRunResult.gasRequired.refTime.replace(/,/g, ''), 10),
+                        proofSize: parseInt(dryRunResult.gasRequired.proofSize.replace(/,/g, ''), 10),
                     }),
                 },
                 ...params
-                ).signAndSend(owner, { nonce: -1 }, async (result: any) => {
-                    console.log(result.status.toHuman());
-                    const isBroadcast = await this.isBroadcast(result.status.isBroadcast);
+                );
+                await await tx.signAndSend(owner, { nonce: -1 }, async (result: any) => {
                     if (result.dispatchError) {
                         if (result.dispatchError.isModule) {
                             const decoded = api.registry.findMetaError(result.dispatchError.asModule);
@@ -111,24 +92,50 @@ export default class TXRepository {
                             reject(result.dispatchError.toString());
                         }
                     } else if (result.status.isInBlock) {
-                        if (!isBroadcast) {
-                            resolve({
-                                status: 200,
-                                message: `${method} in block`,
-                                data: {
-                                    isFinalized: result.status.isFinalized,
-                                    blockHash: result.status.asInBlock.toHex(),
-                                },
-                            });
-                        }
-                    } else if (result.status.isFinalized) {
-                        console.log('finalized');
                         resolve({
                             status: 200,
-                            message: `${method} finalized`,
+                            message: `${method} in block`,
                             data: {
                                 isFinalized: result.status.isFinalized,
-                                blockHash: result.status.asFinalized.toHex(),
+                                blockHash: result.status.asInBlock.toHex(),
+                            },
+                        });
+                    } 
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    static executeExtrinsic = async (
+        api: any,
+        executeExtrinsic: any,
+        rawExtrinsic: any,
+    ) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const dryRunResult = await this.dryRunExtrinsic(api, rawExtrinsic);
+                if (!dryRunResult) {
+                    reject(dryRunResult);
+                }
+                await await executeExtrinsic.send(async (result: any) => {
+                    if (result.dispatchError) {
+                        if (result.dispatchError.isModule) {
+                            const decoded = api.registry.findMetaError(result.dispatchError.asModule);
+                            const { docs, name, section } = decoded;
+                            reject(`${section}.${name}: ${docs.join(' ')}`);
+                        } else {
+                            console.log(result.dispatchError.toString());
+                            reject(result.dispatchError.toString());
+                        }
+                    } else if (result.status.isInBlock) {
+                        resolve({
+                            status: 200,
+                            message: `Transaction in block`,
+                            data: {
+                                isFinalized: result.status.isFinalized,
+                                blockHash: result.status.asInBlock.toHex(),
                             },
                         });
                     }
@@ -146,16 +153,125 @@ export default class TXRepository {
         params: any,
         instance: any
     ) => {
-        const gasLimit = api.registry.createType(
-            'WeightV2',
-            api.consts.system.blockWeights['maxBlock']
-        );
-      
-        const { output } = await contract.query[method](
-            instance.contractAddress,
-            { gasLimit: gasLimit },
+        try {
+            const gasLimit = api.registry.createType(
+                'WeightV2',
+                api.consts.system.blockWeights['maxBlock']
+            );
+            const { output } = await contract.query[method](
+                instance.contractAddress,
+                { gasLimit: gasLimit },
+                ...params
+            );
+            return output?.toJSON();
+        } catch (error) {
+            return error;
+        }
+    }
+
+    static dryRunContract = async (
+        api: any,
+        contract: any,
+        method: any,
+        owner: any,
+        params: any,
+        instance: any,
+        storageDepositLimit: any
+    ) => {
+        try {
+            const { gasRequired, storageDeposit, result, output } = await contract.query[method](
+                owner.address,
+                {
+                    gasLimit: api?.registry.createType('WeightV2', {
+                        refTime: instance.REFTIME,
+                        proofSize: instance.PROOFSIZE,
+                    }),
+                    storageDepositLimit,
+                },
+                ...params
+            );
+            if (result.isErr) {
+                let error: any;
+                if (result.asErr.isModule) {
+                    const dispatchError = api.registry.findMetaError(result.asErr.asModule)
+                    error = dispatchError.docs.length ? dispatchError.docs.concat().toString() : dispatchError.name
+                } else {
+                    error = result.asErr.toString()
+                }
+                throw String(error);
+            }
+            if (result.isOk) {
+                const flags = result.asOk.flags.toHuman()
+                if (flags.includes('Revert')) {
+                    const type = contract.abi.messages[5].returnType
+                    const typeName = type?.lookupName || type?.type || ''
+                    const error = contract.abi.registry.createTypeUnsafe(typeName, [result.asOk.data]).toHuman()
+                    throw String(error ? (error as any).Err : 'Revert')
+                }
+            }
+            return { 
+                gasRequired: gasRequired.toHuman(), 
+                storageDeposit: storageDeposit.toHuman(), 
+                result: result.toHuman(),
+                output: output.toHuman(),
+            };
+        } catch (error) {
+            throw String(error);
+        }
+    }
+
+    static constructContractExtrinsicTransaction = async (
+        api: any,
+        contract: any,
+        method: any,
+        params: any,
+        option: any,
+    ) => {
+        try {
+            const options: any = {
+                storageDepositLimit: null,
+                gasLimit: api.registry.createType('WeightV2', {
+                  refTime: parseInt(option.gasRequired.refTime.replace(/,/g, ''), 10),
+                  proofSize: parseInt(option.gasRequired.proofSize.replace(/,/g, ''), 10),
+                }),
+            };
+            const tx = contract.tx[method](
+                options,
+                ...params
+            )
+            return tx;
+        } catch (error) {
+            throw String(error);
+        }
+    }
+
+    static dryRunTransaction = async (
+        api: any,
+        pallet: any,
+        method: any,
+        owner: any,
+        params: any,
+    ) => {
+        try {
+            const tx =  api.tx[pallet][method](
             ...params
-        );
-        return output?.toJSON();
+            )
+            const result = await api.rpc.system.dryRun(tx);
+            return result;
+        } catch (error) {
+            throw String(error);
+        }
+    }
+
+    static dryRunExtrinsic = async (
+        api: any,
+        rawExtrinsic: any,
+    ) => {
+        try {
+            const result = await api.rpc.system.dryRun(rawExtrinsic);
+            return result;
+        } catch (error) {
+            throw String(error);
+        }
     }
 }
