ea1c897b60e7b71697b9692abfbfa0c4d1ff2d6c
Joel Villena <villenajv43@gmail.com>
2024-05-09 15:20:06 +0800

    cleanup azk

--- src/schemas/AssetSchemas.ts
+++ src/schemas/AssetSchemas.ts
@@ -3,26 +3,10 @@
 export interface IBalanceOfRequestParams {
   account: string;
 }
-export interface IBalanceOfResponseSuccessful {
-  status: number;
-  message: string;
-}
-export interface IBalanceOfResponseError {
-  status: number;
-  message: string;
-}
 
 // ----- TOTAL SUPPLY -----
 export interface ITotalSupplyRequestParams {
   // 
-}
-export interface ITotalSupplyResponseSuccessful {
-  status: number;
-  message: string;
-}
-export interface ITotalSupplyResponseError {
-  status: number;
-  message: string;
 }
 
 // POST
@@ -31,27 +15,11 @@ export interface IMintRequestBody {
   to: string;
   value: number;
 }
-export interface IMintResponseSuccessful {
-  status: number;
-  message: string;
-}
-export interface IMintResponseError {
-  status: number;
-  message: string;
-}
 
 // ----- TRANSFER -----
 export interface ITransferRequestBody {
   target: string;
   value: number;
-}
-export interface ITransferResponseSuccessful {
-  status: number;
-  message: string;
-}
-export interface ITransferResponseError {
-  status: number;
-  message: string;
 }
 
 // DELETE
@@ -60,11 +28,13 @@ export interface IBurnRequestBody {
   from: string;
   value: number;
 }
-export interface IBurnResponseSuccessful {
+
+// ----- SUCCESS & ERROR -----
+export interface IResponseSuccessful {
   status: number;
   message: string;
 }
-export interface IBurnResponseError {
+export interface IResponseError {
   status: number;
   message: string;
 }
