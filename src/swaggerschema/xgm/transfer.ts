754910bcb4599c08910c6f1d252660263ed969c7
Joel Villena <villenajv43@gmail.com>
2024-05-28 16:29:00 +0800

    return hash on xav and xgm transfer

--- src/swaggerschema/xgm/transfer.ts
+++ src/swaggerschema/xgm/transfer.ts
@@ -17,30 +17,22 @@ export const transfer = {
     },
     // Response schema for success
     response: {
-        // 200: {
-        //     description: 'Success response after minting token.',
-        //     type: 'object',
-        //     properties: {
-        //         status: { type: 'number' },
-        //         message: { type: 'string' },
-        //         data: {
-        //             type: 'object',
-        //             properties: {
-        //                 isFinalized: { type: 'boolean' },
-        //                 blockHash: { type: 'string' },
-        //             },
-        //         },
-        //     },
-        // },
-        // // Response schema for unspecified code
-        // default: {
-        //     description: 'Default response',
-        //     type: 'object',
-        //     properties: {
-        //         status: { type: 'number' },
-        //         message: { type: 'string' },
-        //     },
-        // }
+        200: {
+            description: 'Success response after minting token.',
+            type: 'object',
+            properties: {
+                hash: { type: 'string' },
+            },
+        },
+        // Response schema for unspecified code
+        default: {
+            description: 'Default response',
+            type: 'object',
+            properties: {
+                status: { type: 'number' },
+                message: { type: 'string' },
+            },
+        }
     },
     security: [
         {
