7a17c9d419247c9f5398a0d375620834fd0ff442
Joel Villena <villenajv43@gmail.com>
2024-06-27 15:42:47 +0800

    revert basic auth

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ export type AppOptions = {
 
 // Pass --options via CLI arguments in command to enable these options.
 const options: AppOptions = {};
-const crypto = require('node:crypto')
+
 const app: FastifyPluginAsync<AppOptions> = async (
   fastify: any,
   opts
@@ -107,32 +107,6 @@ const app: FastifyPluginAsync<AppOptions> = async (
     }
   })
 
-  function compare (a: any, b: any) {
-    a = Buffer.from(a)
-    b = Buffer.from(b)
-    if (a.length !== b.length) {
-      crypto.timingSafeEqual(a, a)
-      return false
-    }
-    return crypto.timingSafeEqual(a, b)
-  }
-
-  const user = process.env.USERNAME == undefined ? 'NEXGEN' : process.env.USERNAME;
-  const pass = process.env.PASSWORD == undefined ? '@1234Abcd' : process.env.PASSWORD;
-  await fastify.register(require('@fastify/basic-auth'), {
-    validate (username: any, password: any, req: any, reply: any, done: any) {
-      let result = true
-      result = compare(username, user) && result
-      result = compare(password, pass) && result
-      if (result) {
-        done()
-      } else {
-        done(new Error('Access denied'))
-      }
-    },
-    authenticate: true
-  })
-
   await fastify.register(require('@fastify/swagger-ui'), {
     routePrefix: '/docs',
     uiConfig: {
@@ -140,7 +114,7 @@ const app: FastifyPluginAsync<AppOptions> = async (
       deepLinking: false
     },
     uiHooks: {
-      onRequest: fastify.basicAuth,
+      onRequest: function (request: any, reply: any, next: () => void) { next() },
       preHandler: function (request: any, reply: any, next: () => void) { next() }
     },
     staticCSP: true,
