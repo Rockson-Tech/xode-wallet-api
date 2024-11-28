ffbcdd1e5050a156e885bf25f1c6a813c4b0605a
Joel Villena <villenajv43@gmail.com>
2024-11-28 10:15:41 +0800

    Update SampleController.ts

--- src/controllers/SampleController.ts
+++ src/controllers/SampleController.ts
@@ -1,36 +1,36 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
-import SampleRepository from '../repositories/SampleRepository';
-import { ApiPromise, WsProvider } from '@polkadot/api';
+// import { FastifyReply, FastifyRequest } from 'fastify';
+// import SampleRepository from '../repositories/SampleRepository';
+// import { ApiPromise, WsProvider } from '@polkadot/api';
 
-let api: ApiPromise;
+// let api: ApiPromise;
 
-export const sampleController = async (
-	request: FastifyRequest,
-	reply: FastifyReply
-) => {
-	console.log('sampleController called');
-	try {
-		console.log(`Is it connected? ${api}`)
-		if (!api) {
-			const customRPC = {
-				balances: {
-					transferKeepAlive: {
-						description: 'Custom transferKeepAlive RPC method',
-						params: [
-							{ name: 'account', type: 'AccountId' },
-							{ name: 'amount', type: 'u128' },
-							{ name: 'at', type: 'BlockHash', isOptional: true }
-						],
-						type: 'String'
-					}
-				}
-			}
-			const wsProvider = new WsProvider('wss://rpc1.paseo.popnetwork.xyz');
-			api = await ApiPromise.create({provider: wsProvider, rpc: customRPC});
-		}
-		const result = await SampleRepository.sampleRepo(api);
-		console.log(result);
-	} catch (error: any) {
-		reply.status(500).send('Internal Server Error: ' + error);
-	}
-};
+// export const sampleController = async (
+// 	request: FastifyRequest,
+// 	reply: FastifyReply
+// ) => {
+// 	console.log('sampleController called');
+// 	try {
+// 		console.log(`Is it connected? ${api}`)
+// 		if (!api) {
+// 			const customRPC = {
+// 				balances: {
+// 					transferKeepAlive: {
+// 						description: 'Custom transferKeepAlive RPC method',
+// 						params: [
+// 							{ name: 'account', type: 'AccountId' },
+// 							{ name: 'amount', type: 'u128' },
+// 							{ name: 'at', type: 'BlockHash', isOptional: true }
+// 						],
+// 						type: 'String'
+// 					}
+// 				}
+// 			}
+// 			const wsProvider = new WsProvider('wss://rpc1.paseo.popnetwork.xyz');
+// 			api = await ApiPromise.create({provider: wsProvider, rpc: customRPC});
+// 		}
+// 		const result = await SampleRepository.sampleRepo(api);
+// 		console.log(result);
+// 	} catch (error: any) {
+// 		reply.status(500).send('Internal Server Error: ' + error);
+// 	}
+// };
